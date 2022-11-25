7b11bd9dd1bf1604d61bc704f72bc493e3e122f9
hamzafarooq360 <86162690+hamzafarooq360@users.noreply.github.com>
2022-11-25 15:14:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, Response
-from camera import VideoCamera
-
+from video import VideoCamera
+from video import Model
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
-def gen(camera):
+def gen(video):
     while True:
-        frame = camera.get_frame()
+        frame = video.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -20,4 +20,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
